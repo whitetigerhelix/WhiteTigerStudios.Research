aaf2e1d5a7d3236ac16ad3266c9508f71b7ddc6e
Chris Oje <chrisoje@helionoctis.com>
2023-05-29 15:20:31 -0700

    Player is working with movement

--- WhiteTigerStudios.Research-Unity/Packages/whitetigerstudios.research.platformer/Scripts/PlatformerPlayer.cs
+++ WhiteTigerStudios.Research-Unity/Packages/whitetigerstudios.research.platformer/Scripts/PlatformerPlayer.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 namespace WhitetigerStudios.Research.Platformer
 {
     /// <summary>
-    /// The player that moves around the world
+    /// Player movement, etc
     /// </summary>
     public class PlatformerPlayer : MonoBehaviour
     {
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float jumpImpulse = 10f;
+        [SerializeField] private Rigidbody rigidBody;
+
+        private void FixedUpdate()
+        {
+            // Walk
+            float moveAmount = Input.GetAxisRaw("Horizontal");
+            Vector3 movement = (Vector3.forward * moveAmount).normalized * moveSpeed * Time.fixedDeltaTime;
+            rigidBody.MovePosition(rigidBody.position + movement);
+
+            // Jump
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Vector3 jumpMovement = Vector3.up * jumpImpulse;
+                rigidBody.AddForce(jumpMovement, ForceMode.Impulse);
+            }
+        }
     }
 }
