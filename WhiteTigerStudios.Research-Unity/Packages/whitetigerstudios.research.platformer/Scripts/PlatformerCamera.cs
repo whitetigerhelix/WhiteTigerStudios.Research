6bf3742f24cdac6e8ed8dbcd9943bfe301b450f1
Chris Oje <chrisoje@helionoctis.com>
2023-05-29 15:55:09 -0700

    Got a sidescroller camera working

--- WhiteTigerStudios.Research-Unity/Packages/whitetigerstudios.research.platformer/Scripts/PlatformerCamera.cs
+++ WhiteTigerStudios.Research-Unity/Packages/whitetigerstudios.research.platformer/Scripts/PlatformerCamera.cs
@@ -8,5 +8,36 @@ namespace WhitetigerStudios.Research.Platformer
     /// </summary>
     public class PlatformerCamera : MonoBehaviour
     {
+        [SerializeField] private PlatformerPlayer player;
+        [SerializeField] private float followSpeed = 5f;
+        [SerializeField] private float minZoom = 5f;
+        [SerializeField] private float maxZoom = 25f;
+        [SerializeField] private float zoomSpeed = 10f;
+        [SerializeField] private float easingAmount = 0.1f;
+
+        [Header("Camera")]
+        [SerializeField] private Vector3 offsetFromPlayer = new(5f, 3f, 0f);
+        [SerializeField] private bool doZoomForSpeed = false;
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            // Follow target
+            Vector3 targetPos = player.transform.position + offsetFromPlayer;
+            transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+
+            // Zoom based on target's speed
+            if (doZoomForSpeed)
+            {
+                float zoomLevel = Mathf.Lerp(maxZoom, minZoom, player.RigidBody.velocity.magnitude / zoomSpeed);
+                _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, zoomLevel, easingAmount);
+            }
+        }
     }
 }
--- WhiteTigerStudios.Research-Unity/Packages/whitetigerstudios.research.platformer/Scripts/PlatformerPlayer.cs
+++ WhiteTigerStudios.Research-Unity/Packages/whitetigerstudios.research.platformer/Scripts/PlatformerPlayer.cs
@@ -8,19 +8,24 @@ namespace WhitetigerStudios.Research.Platformer
     /// </summary>
     public class PlatformerPlayer : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 5f;
-        [SerializeField] private float jumpImpulse = 10f;
+        [SerializeField] private float moveSpeed = 200f;
+        [SerializeField] private float jumpImpulse = 25f;
+        //[SerializeField] private float smoothness = 1f;     // Smooth movement
         [SerializeField] private Rigidbody rigidBody;
+        public Rigidbody RigidBody => rigidBody;
 
         private void FixedUpdate()
         {
             // Walk
             float moveAmount = Input.GetAxisRaw("Horizontal");
+            //float smoothedMoveAmount = Mathf.Lerp(currentMoveAmount, moveAmount, smoothness * Time.fixedDeltaTime);
+            //Vector3 movement = (Vector3.forward * smoothedMoveAmount).normalized * moveSpeed * Time.fixedDeltaTime;
             Vector3 movement = (Vector3.forward * moveAmount).normalized * moveSpeed * Time.fixedDeltaTime;
-            rigidBody.MovePosition(rigidBody.position + movement);
+            movement.y = rigidBody.velocity.y;
+            rigidBody.velocity = movement;
 
             // Jump
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
             {
                 Vector3 jumpMovement = Vector3.up * jumpImpulse;
                 rigidBody.AddForce(jumpMovement, ForceMode.Impulse);
